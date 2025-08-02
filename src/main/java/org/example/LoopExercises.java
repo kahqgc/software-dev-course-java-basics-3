df9b7298eea80353f20cbec2f93753c5cd790be3
kahqgc <kahqgc@gmail.com>
2025-08-02 13:10:02 -0500

    fixed loop for sum

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -5,7 +5,7 @@ public class LoopExercises {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // (use a for loop)
         int total = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             total +=i;
         }
         return total;
